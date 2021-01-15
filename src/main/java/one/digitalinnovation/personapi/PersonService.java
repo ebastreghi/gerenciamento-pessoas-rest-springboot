011622e49cc84f39c3f26c81c9022b184e14bbe1
edevarfilho <edevarfilho@gmail.com>
2021-01-15 16:25:20 -0300

    save, validation, getAll

--- src/main/java/one/digitalinnovation/personapi/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/PersonService.java
@@ -3,22 +3,34 @@ package one.digitalinnovation.personapi;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
     private PersonRepository personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     public PersonService(PersonRepository personRepository){
         this.personRepository = personRepository;
     }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO){
-        Person savedPerson = personRepository.save(personDTO);
+        Person personToSave = personMapper.toModel(personDTO);
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder()
                 .message("Created penson with ID " + savedPerson.getId())
                 .build();
+    }
+
+
+    public List<PersonDTO> listAll() {
+        return personRepository.findAll().stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
